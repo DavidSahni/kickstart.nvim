ac18bd1ff1e313b7ec974f052b711d5ee29b4b5e
David Sahni <David.Sahni02@gmail.com>
2026-04-16 16:26:58 -0700

    Added support for .ts file formatting, added ts inlay hints

--- init.lua
+++ init.lua
@@ -756,7 +756,21 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        ts_ls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                -- includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+                -- includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+              },
+            },
+          },
+        },
         basedpyright = { settings = { basedpyright = { analysis = { typeCheckingMode = 'basic' } } } },
         --
       }
@@ -856,6 +870,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
         javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
